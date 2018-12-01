0b97a8e60a0b7fe7afa938b9f73a542b659865c7
00095417 <00095417@uca.edu.sv>
2018-12-01 15:46:29 -0600

    trabajando las peticiones al servidor de la api

--- routes/bestiario.js
+++ routes/bestiario.js
@@ -2,7 +2,13 @@ var express = require('express');
 var bestiarioController = require('../controllers/bestiarioController');
 var bestiario = express.Router();
 
-//metodo get para más de un usuario
+//metodo get para más de un elemento
 bestiario.get('/',bestiarioController.get);
-//metodo get para un usario
+//metodo get para un elemento
 bestiario.get('/:id',bestiarioController.get);
+//metodo para agregar un elemento
+bestiario.post('/',bestiarioController.post);
+//metodo para actualizar un elemento
+bestiario.put('/:id',bestiarioController.put);
+//metodo para eliminar un elemento
+bestiario.delete('/:id'.bestiarioController.delete);
